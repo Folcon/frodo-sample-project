683e2e30352115177d2b90fe2294e215a3f1654b
Folcon Red <folcon@gmail.com>
2014-02-09 21:43:13 +0000

    init commit to hopefully show the problem

---
+++ src/clj/sample_project/core.clj
@@ -0,0 +1,46 @@
+(ns sample-project.core
+  (:require
+    [ring.util.response :refer [response]]
+    [chord.http-kit :refer [with-channel]]
+    [clojure.core.async :refer [<! >! put! close! go-loop]]
+    [compojure.core :refer [defroutes GET routes]]
+    [compojure.handler :refer [api]]
+    [compojure.route :refer [resources]]
+    [hiccup.page :refer [html5 include-js]]
+    [hiccup.element :refer [javascript-tag]]
+    ))
+
+(def port (Integer. (or (System/getenv "PORT") 3000)))
+
+(defn index-page []
+  (html5
+   [:head
+    [:title "London Clojure Dojo January 2014"]]
+    [:body
+      [:div#app]
+      (include-js "//fb.me/react-0.8.0.js") ; only required in dev build
+      (include-js "/out/goog/base.js") ; only required in dev build
+      (include-js "/js/sample-project.js")
+      (javascript-tag "goog.require('sample_project.client');") ; only required in dev build
+      ]))
+
+(defn ws-handler [req]
+  (with-channel req ws
+    (println "Opened connection from" (:remote-addr req))
+    (go-loop []
+      (when-let [{:keys [message]} (<! ws)]
+        (println "Message received:" message)
+        (>! ws (format "You said: '%s' at %s." message (java.util.Date.)))
+        (recur)))))
+
+(defn app-routes []
+  (routes
+    (GET "/" [] (response (index-page)))
+    (GET "/ws" [] ws-handler)
+    (resources "/js" {:root "js"})
+    (resources "/out" {:root "out"}) ; only required in dev build
+    ))
+
+(defn -main []
+  (-> (app-routes)
+      api))
---
+++ src/cljs/sample_project/cljs/client.cljs
@@ -0,0 +1,74 @@
+(ns sample-project.client
+    (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:refer-clojure :exclude [chars])
+  (:require
+    [cljs.core.async :refer [<! >! put! close! timeout]]
+    [chord.client :refer [ws-ch]]
+    [om.core :as om :include-macros true]
+    [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+; debugging
+; log to console
+; (prn "test")
+; trigger javascript breakpoint
+; (js* "debugger;")
+
+(defn save-message [e owner]
+  (om/set-state! owner :message-to-send (.. e -target -value)))
+
+(defn send-message [ws e owner]
+  (put! ws (om/get-state owner :message-to-send))
+  (om/set-state! owner :message-to-send "")
+  (.preventDefault e))
+
+(defn the-sender [{:keys [ws]} owner]
+  (reify
+    om/IInitState
+    (init-state [_] {:message-to-send ""})
+    om/IRender
+    (render [_]
+      (dom/form #js {:onSubmit #(send-message ws % owner)}
+      (dom/h3 nil "Send message to server")
+      (dom/input
+          #js {:type "text"
+               :ref "text-field"
+               :value (om/get-state owner :message-to-send)
+               :onChange #(save-message % owner)})))))
+
+(defn message [{:keys [message]} owner]
+  (om/component
+    (dom/li nil message)))
+
+(defn listen-for-mesages [{:keys [ws messages]}]
+  (go-loop []
+    (when-let [msg (<! ws)]
+      (om/update! messages conj msg)
+      (recur))))
+
+(defn message-list [data owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (listen-for-mesages data))
+    om/IRender
+    (render [_]
+      (dom/div nil
+        (dom/h3 nil "Messages received from server")
+        (dom/ul nil
+          (om/build-all message (data :messages)))))))
+
+(go
+  (let [ws (<! (ws-ch "ws://localhost:3000/ws")) ; establish web socket connection
+        app-state (atom {:ws ws :messages [{:message "None Yet!"}]})] ; initialise global state of app
+        (om/root
+          app-state
+          (fn [app owner]
+            (reify
+              om/IRender
+              (render [_]
+                (dom/div nil
+                  (om/build the-sender app)
+                  (om/build message-list app)))))
+          (.getElementById js/document "app"))))
